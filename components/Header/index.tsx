42a59766dec8508f6d21e59412daaf0c60740503
vitalii-minchuk <mcmin4doutsch@gmail.com>
2022-09-22 22:51:14 +0300

    basic structure

---
+++ components/Header/index.tsx
@@ -0,0 +1,15 @@
+function Header() {
+  return (
+    <header className="text-lg text-red-600">
+      <div>
+        <div></div>
+        <div>
+          <div></div>
+          <p>get in touch</p>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{tsx,ts,jsx,js}",
+    "./components/**/*.{tsx,ts,jsx,js}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
